ec7edfadcfab2e131c70fed23b17332415b8eacb
christophszcz <christoph.szczechowicz@gmail.com>
2017-01-30 16:53:08 -0500

    The getJSON method now works.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-   $("#getsearches").on("click", function(){
-
+   $("#getSearches").on("click", function(){
+	  $.getJSON(" https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", function(json) {
+	  	$(".container").html(JSON.stringify(json));
+		});
 	});
 });
