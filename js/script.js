0e6c6806e4899848f4f313178ef0b4e2a7884d31
christophszcz <christoph.szczechowicz@gmail.com>
2017-01-31 17:30:01 -0500

    Now the search term is retrieved.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-   $("#getSearches").on("click", function(){
-	  $.getJSON(" https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", function(json) {
-	  	$(".container").html(JSON.stringify(json));
-		});
+  $("#getSearches").on("click", function(){
+  	var search = $('#searchfield').val();
+  	console.log(search);
 	});
 });
+ 
