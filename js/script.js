02b07bc39b4722419a50bd65a36abf22028fc952
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-01 00:44:42 -0500

    The API now does it's intended function.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
   $("#getSearches").on("click", function(){
   	var search = $('#searchfield').val();
-  	console.log(search);
+	  $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=10&formate=json&origin=*", function(json) {
+	 	var html = "";
+    for(var i = 0; i < 10; i++){
+      html += "<div class = 'text'>";
+      html += "<p>" + json[1][i] + "</p>";
+      html +="<a href='"  + json[3][i] +"'>" + json[2][i]+ "</>";
+      html += "</div>";
+    }
+		$(".container").html(html);
+		});
 	});
 });
  
