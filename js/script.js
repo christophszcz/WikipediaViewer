d30c362afc70f48363d3ce69b3021061c9bbd827
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-01 10:33:39 -0500

    Added search result styling.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
 	  $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=10&formate=json&origin=*", function(json) {
 	 	var html = "";
     for(var i = 0; i < 10; i++){
-      html += "<div class = 'text'>";
-      html += "<p>" + json[1][i] + "</p>";
-      html +="<a href='"  + json[3][i] +"'>" + json[2][i]+ "</>";
-      html += "</div>";
+      html += "<div class = 'search-results'>";
+      html +="<a href='"  + json[3][i] +"' class='description'>"
+      html +="<p class='result-title'>" + json[1][i] + "</p>"; 
+      html += json[2][i]+ "</a>";
+      html +="<hr/>";
+      html +="</div>";
     }
 		$(".container").html(html);
 		});
