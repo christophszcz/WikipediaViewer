b6e9fb87f449cb1128aa6e8c6386c4c92142e69f
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-01 15:50:12 -0500

    Added space after the searches.

--- js/script.js
+++ js/script.js
@@ -4,25 +4,25 @@ $(document).ready(function() {
      $('#getSearches').click(); 
     }
   });
-  
-  $("#getSearches").on("click", function(){
-  	search = $('#searchfield').val();
+
+  $("#getSearches").on("click", function searching(){
+  	var search = $('#searchfield').val();
 	  $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=10&formate=json&origin=*", function(json) {
-	 	var html = "";
-    for(var i = 0; i < 10; i++){
-      html += "<div class = 'search-results'>";
-      html +="<a href='"  + json[3][i] +"' class='description' target='_blank'>"
-      html +="<p class='result-title'>" + json[1][i] + "</p>"; 
-      html += json[2][i]+ "</a>";
-      html +="</div>";
-      html +="<hr/>";
-    }
-		$(".container").html(html);
-    $(document.body).prepend(
-        "<div class='col-lg-6 col-lg-offset-3 group'><div class='input-group'><input id='searchfield' type='search' class='form-control' placeholder='Search for...' value='" 
-        + json[0] + 
-        "'><span class='input-group-btn'><button class='btn btn-default' id='getSearches' type='button'><div class='glyphicon glyphicon-search'></div></button></span></div></div>");
+  	 	var html = "";
+
+      for(var i = 0; i < 10; i++){
+        html += "<div class = 'search-item'>";
+        html +="<a href='"  + json[3][i] +"' class='description' target='_blank'>"
+        html +="<p class='result-title'>" + json[1][i] + "</p>"; 
+        html += json[2][i]+ "</a>";
+        html +="</div>";
+        html +="<hr/>";
+      }
+  		$(".search-results").html(html);
+
+      if(!($('#instruction').length > 0)){
+        $('.search-results').addClass('space');
+      }
 		});
 	});
 });
- 
