9625d4499764917a535f9441643ecb890613e110
christophszcz <christoph.szczechowicz@gmail.com>
2017-09-03 23:36:48 -0400

    Remove any undefined entries from the search results.

--- js/script.js
+++ js/script.js
@@ -29,18 +29,22 @@ $(document).ready(function() {
 	  $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=10&formate=json&origin=*", function(json) {
   	 	var html = "";
 
-      if (json[2][0] == undefined){
+      if (json[1][0] == undefined){
         html += "<div>";
         html +="<p>No Search Results</p>"; 
         html +="</div>"
       }else{
         for(var i = 0; i < 10; i++){
-          html += "<div class = 'search-item'>";
-          html +="<a href='"  + json[3][i] +"' class='description' target='_blank'>"
-          html +="<p class='result-title'>" + json[1][i] + "</p>"; 
-          html += json[2][i]+ "</a>";
-          html +="</div>";
-          html +="<hr/>";
+          if (json[1][i] === undefined){
+            html +="";
+          } else {
+            html += "<div class = 'search-item'>";
+            html +="<a href='"  + json[3][i] +"' class='description' target='_blank'>"
+            html +="<p class='result-title'>" + json[1][i] + "</p>"; 
+            html += json[2][i]+ "</a>";
+            html +="</div>";
+            html +="<hr/>";
+          }
         }
       }
 
